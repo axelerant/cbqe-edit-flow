43d896226f9e25432885fbe735af371749eaa6a8
Michael Cannon <mc@aihr.us>
2014-02-18 09:38:01 +0700

    Update localization

--- includes/requirements.php
+++ includes/requirements.php
@@ -22,19 +22,19 @@ if ( ! function_exists( 'aihr_notice_version' ) ) {
 	function aihr_notice_version( $required_base, $required_name, $required_slug, $required_version, $item_name ) {
 		$is_active = is_plugin_active( $required_base );
 		if ( $is_active )
-			$link = sprintf( __( '<a href="%1$s">update to</a>', 'gc-testimonials-to-testimonials' ), self_admin_url( 'update-core.php' ) );
+			$link = sprintf( __( '<a href="%1$s">update to</a>', 'gc-testimonials-to-testimonials', 'cbqe-edit-flow' ), self_admin_url( 'update-core.php' ) );
 		else {
 			$plugins = get_plugins();
 			if ( empty( $plugins[ $required_base ] ) ) {
 				$install = esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=' . $required_slug ), 'install-plugin_' . $required_slug ) );
-				$link    = sprintf( __( '<a href="%1$s">install</a>', 'gc-testimonials-to-testimonials' ), $install );
+				$link    = sprintf( __( '<a href="%1$s">install</a>', 'gc-testimonials-to-testimonials', 'cbqe-edit-flow' ), $install );
 			} else {
 				$activate = esc_url( wp_nonce_url( admin_url( 'plugins.php?action=activate&plugin=' . $required_base ), 'activate-plugin_' . $required_base ) );
-				$link     = sprintf( __( '<a href="%1$s">activate</a>', 'gc-testimonials-to-testimonials' ), $activate );
+				$link     = sprintf( __( '<a href="%1$s">activate</a>', 'gc-testimonials-to-testimonials', 'cbqe-edit-flow' ), $activate );
 			}
 		}
 
-		$text = sprintf( __( 'Plugin "%3$s" has been deactivated. Please %1$s "%4$s" version %2$s or newer before activating "%3$s".', 'gc-testimonials-to-testimonials' ), $link, $required_version, $item_name, $required_name );
+		$text = sprintf( __( 'Plugin "%3$s" has been deactivated. Please %1$s "%4$s" version %2$s or newer before activating "%3$s".', 'gc-testimonials-to-testimonials', 'cbqe-edit-flow' ), $link, $required_version, $item_name, $required_name );
 
 		$content  = '<div class="error"><p>';
 		$content .= $text;
