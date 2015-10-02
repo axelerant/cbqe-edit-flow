56055980c06c06c697634a6780d87230d143ec9f
Saurabh Dixit <saurabhsirdixit@gmail.com>
2015-10-02 17:56:22 +0530

    Modified code to check activation helpers via class instead of hard coded plugin name

--- includes/requirements.php
+++ includes/requirements.php
@@ -19,22 +19,8 @@
 require_once ABSPATH . 'wp-admin/includes/plugin.php';
 
 if ( ! function_exists( 'aihr_notice_version' ) ) {
-	function aihr_notice_version( $required_base, $required_name, $required_slug, $required_version, $item_name ) {
-		$is_active = is_plugin_active( $required_base );
-		if ( $is_active ) {
-			$link = sprintf( __( '<a href="%1$s">update to</a>', 'gc-testimonials-to-testimonials', 'cbqe-edit-flow' ), self_admin_url( 'update-core.php' ) );
-		} else {
-			$plugins = get_plugins();
-			if ( empty( $plugins[ $required_base ] ) ) {
-				$install = esc_url( wp_nonce_url( self_admin_url( 'update.php?action=install-plugin&plugin=' . $required_slug ), 'install-plugin_' . $required_slug ) );
-				$link    = sprintf( __( '<a href="%1$s">install</a>', 'gc-testimonials-to-testimonials', 'cbqe-edit-flow' ), $install );
-			} else {
-				$activate = esc_url( wp_nonce_url( admin_url( 'plugins.php?action=activate&plugin=' . $required_base ), 'activate-plugin_' . $required_base ) );
-				$link     = sprintf( __( '<a href="%1$s">activate</a>', 'gc-testimonials-to-testimonials', 'cbqe-edit-flow' ), $activate );
-			}
-		}
-
-		$text = sprintf( __( 'Plugin "%3$s" has been deactivated. Please %1$s "%4$s" version %2$s or newer before activating "%3$s".', 'gc-testimonials-to-testimonials', 'cbqe-edit-flow' ), $link, $required_version, $item_name, $required_name );
+	function aihr_notice_version( $required_name, $required_version, $item_name ) {
+		$text = sprintf( __( 'Plugin "%2$s" has been deactivated. Please install and activate "%3$s" version %1$s or newer before activating "%2$s".' ), $required_version, $item_name, $required_name );
 
 		$content  = '<div class="error"><p>';
 		$content .= $text;
@@ -48,7 +34,7 @@ if ( ! function_exists( 'aihr_notice_version' ) ) {
 
 function cbqe_ef_requirements_check() {
 	$check_okay = true;
-	if ( ! is_plugin_active( CBQE_EF_REQ_BASE ) && ! is_plugin_active( CBQE_EF_REQ_BASE_PREM ) ) {
+	if ( ! class_exists( CBQE_EF_REQ_CLASS ) ) {
 		add_action( 'admin_notices', 'cbqe_ef_notice_version' );
 
 		$check_okay = false;
@@ -65,14 +51,14 @@ function cbqe_ef_requirements_check() {
 
 
 function cbqe_ef_notice_version() {
-	aihr_notice_version( CBQE_EF_REQ_BASE, CBQE_EF_REQ_NAME, CBQE_EF_REQ_SLUG, CBQE_EF_REQ_VERSION, CBQE_EF_NAME );
+	aihr_notice_version( CBQE_EF_REQ_NAME, CBQE_EF_REQ_VERSION, CBQE_EF_NAME );
 
 	deactivate_plugins( CBQE_EF_BASE );
 }
 
 
 function cbqe_ef_notice_version_ef() {
-	aihr_notice_version( CBQE_EF_EXT_BASE, CBQE_EF_EXT_NAME, CBQE_EF_EXT_SLUG, CBQE_EF_EXT_VERSION, CBQE_EF_NAME );
+	aihr_notice_version( CBQE_EF_EXT_NAME, CBQE_EF_EXT_VERSION, CBQE_EF_NAME );
 
 	deactivate_plugins( CBQE_EF_BASE );
 }
